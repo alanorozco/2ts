fe791ed3cb867d9b7c5a12dfbba206a8bb15f9b4
Alan Orozco <alan@orozco.xyz>
2022-02-18 12:54:40 -0800

    clean extension

--- test/util.mjs
+++ test/util.mjs
@@ -1,7 +1,8 @@
 import { globbySync } from 'globby';
+import path from 'path';
 
 export const getTestFixtures = () =>
   globbySync('test/fixtures/**/*.js').map((input) => ({
     input,
-    output: `${input}.tsx`,
+    output: `${input.replace(path.extname(input), '')}.tsx`,
   }));
