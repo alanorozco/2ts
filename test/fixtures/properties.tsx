6cc78df394383654d82f24095ed5f1c9c8d7eee7
Alan Orozco <alan@orozco.xyz>
2022-02-18 17:12:50 -0800

    result

--- test/fixtures/properties.tsx
+++ test/fixtures/properties.tsx
@@ -4,8 +4,6 @@ export const Properties = {
   bar: function (bar: string): string {
     return bar;
   },
-  // BUG: Type annotations are ignored
-  // BUG: Comment should be stripped out
 
   baz(bar: string): string {
     return bar;
